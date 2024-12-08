3069ad28a26825cf71c38de75919d7f878d1f10a
francoleon42 <francoleonadress@gmail.com>
2024-12-08 17:12:48 -0300

    responsive mobile

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
   return (
     <section id='projects' className='projects container'>
       <div className='projects__content grid'>
-        <h2>Proyectos personales</h2>
+        <h2>Proyectos</h2>
 
         <div className="card__container grid">
           {projects.map(({ img, title, skillsBackend, skillsFrontend, links }, index) => {
